d97885618422d80446a88e9408764f73b32b146f
Avanish Kumar Patel <150577239+Avanish9090@users.noreply.github.com>
2025-09-17 11:21:32 +0530

    Update Latest.jsx

--- frontend/src/components/Latest.jsx
+++ frontend/src/components/Latest.jsx
@@ -22,62 +22,49 @@ function Latest() {
   }, []);
 
   var settings = {
-    dots: true,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    initialSlide: 0,
-    responsive: [
-      {
-        breakpoint: 1600,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
+  dots: true,
+  infinite: false,
+  speed: 500,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1280,
+      settings: {
+        slidesToShow: 3,
       },
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          infinite: true,
-          dots: true,
-        },
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
       },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
       },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
       },
-    ],
-  };
+    },
+  ],
+};
 
   return (
     <>
-      <div className="border border-gray-400 rounded shadow-lg  bg-white my-10 p-2 mx-auto">
+      <div className="border border-gray-400 rounded shadow-lg  my-10 p-2 mx-auto">
         <h1 className="text-4xl text-center font-black my-3">
           Latest <span className="text-orange-400">Sweets</span> !!
         </h1>
-        <div className="my-10 w-full overflow-x-hidden">
+        <div className="my-10 ">
           <Slider {...settings}>
             {latest.map((item) => (
-       <div className="bg-white rounded-lg shadow-md p-4 w-full h-full">
-        <Card item={item} key={item.id} />
-      </div>
-             
+             <Card item={item} key={item.id} />      
             ))}
           </Slider>
         </div>

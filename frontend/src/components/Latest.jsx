174273a21275b301ae8ab67406279b7913faa6ee
Avanish Kumar Patel <150577239+Avanish9090@users.noreply.github.com>
2025-09-18 08:32:24 +0530

    Update Latest.jsx

--- frontend/src/components/Latest.jsx
+++ frontend/src/components/Latest.jsx
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Slider from "react-slick";
 import Card from "./Card";
+import Responsive from "../hooks/useResponsiveSlides";
 
 function Latest() {
   const [latest, setlatest] = useState([]);
+  const slideResponsive = Responsive();
 
   useEffect(() => {
     const getLatest = async () => {
@@ -19,39 +21,16 @@ function Latest() {
     getLatest();
   }, []);
 
-var settings = {
+ var settings = {
     dots: true,
-    infinite: false,
+    infinite: true,
     speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    initialSlide: 0,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+    slidesToShow: slideResponsive,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: true,
+  };
   };
 
   return (

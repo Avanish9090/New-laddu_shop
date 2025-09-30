f7c3c32cff264167fed091dc39d2f0180c3a8c76
Avanish Kumar Patel <150577239+Avanish9090@users.noreply.github.com>
2025-09-30 11:05:12 +0530

    Update Latest.jsx

--- frontend/src/components/Latest.jsx
+++ frontend/src/components/Latest.jsx
@@ -43,7 +43,37 @@ function Latest() {
         </h1>
         <div className="my-10  slider-container">
           {
-            loading ? ( <p className="text-center font-semibold text-xl">Loading...</p>) : (
+            loading ? (
+              <div className="flex justify-center items-center">
+                <button
+                type="button"
+                className="flex items-center bg-orange-500 text-white font-medium py-2 px-4 rounded disabled:opacity-50"
+                disabled
+              >
+                <svg
+                  className="mr-2 h-5 w-5 animate-spin text-white"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                  />
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"
+                  />
+                </svg>
+                Processing…
+              </button>
+              </div>
+            ) : (
                <Slider {...settings}>
             {latest.map((item) => (
              <Card item={item} key={item.id} />      

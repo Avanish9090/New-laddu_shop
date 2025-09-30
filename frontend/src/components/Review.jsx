029732a4c2ec23bada15087e8882f0a93f0094fa
Avanish Kumar Patel <150577239+Avanish9090@users.noreply.github.com>
2025-09-30 11:14:39 +0530

    Update Review.jsx

--- frontend/src/components/Review.jsx
+++ frontend/src/components/Review.jsx
@@ -8,6 +8,7 @@ import ResponsiveSlides from "../hooks/useResponsiveSlides";
 function Review() {
   const [Rdata, setRdata] = useState([]);
    const responsive = ResponsiveSlides();
+  const [loading , setLoading] = useState(true)
 
    var settings = {
     dots: true,
@@ -27,6 +28,8 @@ function Review() {
         setRdata(res.data.allReviews);
       } catch (err) {
         console.log(err);
+      } finally {
+        setLoading(false)
       }
     };
     getReviews();
@@ -38,11 +41,44 @@ function Review() {
           Customers <span className="text-orange-400">Review</span> !!
         </h1>
         <div className="py-5">
-          <Slider {...settings}>
+          {loading ? (
+            <div className="flex justify-center items-center">
+                <button
+                type="button"
+                className="flex items-center bg-orange-500 text-white font-medium py-2 px-4 rounded disabled:opacity-50"
+                disabled
+              >
+                <svg
+                  className="mr-2 h-5 w-5 animate-spin text-white"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                  />
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"
+                  />
+                </svg>
+                Loading…
+              </button>
+              </div>
+          ) :(
+             <Slider {...settings}>
             {Rdata.map((item) => (
               <ReviewCard item={item} key={item.id} />
             ))}
           </Slider>
+          )}
+         
         </div>
 
         <div className="flex items-center justify-center mt-8">
